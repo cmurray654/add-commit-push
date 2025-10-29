d3ffae7c6a6fb114804d945333c6de3151dda2f7
Charlie Murray <cmurrayj1212@icloud.com>
2025-10-29 10:37:34 -0500

    "update files"

--- add-commit-push.py
+++ add-commit-push.py
@@ -6,3 +6,8 @@ subprocess.run(["git", "status"])
 print ("git add -A")
 subprocess.run(["git", "add", "-A"])
 
+print ("git commit -m")
+subprocess.run(["git", "commit", "-m",  "\"update files\""])
+
+print ("git push")
+subprocess.run(["git", "push"])
